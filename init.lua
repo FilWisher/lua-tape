793601393017bb2092e2c821d10dd1925d503cea
FilWisher <williamfisher@hotmail.co.uk>
2016-03-20 18:03:16 +0000

    adds init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+  --[[lit-meta
+    name = "lua-tape"
+    version = "0.0.1"
+    dependencies = {}
+    description = "A clone of substack/tape for luvit"
+    tags = { "testing", "tap", "tape", "test" }
+    license = "MIT"
+    author = { name = "FilWisher", email = "williamsykesfisher@gmail.com" }
+    homepage = "https://github.com/filwisher/lua-tape"
+  ]]
+  
