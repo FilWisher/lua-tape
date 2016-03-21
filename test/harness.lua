de499aa8c19deddd245fca4ac0e9dca65ac2ad2b
FilWisher <williamfisher@hotmail.co.uk>
2016-03-21 01:25:13 +0000

    removes unused test folder

--- test/harness.lua
+++
@@ -1,9 +0,0 @@
-local test = require('lua-tape')
-
-test('string', function (t) 
-
-  t.ok('caal', 'it is cool')
-  t.equals(8, 8, 'they are equal')
-  t.equals(1, 8, 'they are also equal')
-  t.done()
-end)
